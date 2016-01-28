f063f8d0223a9e52f0562108e6d9e39e495c1e89
usmaks <anoly.konz.0099@gmail.com>
2016-01-29 07:24:28 +0900

    modifed

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/about', middleware.requireAuthentication, function(req, res){
 	res.send('About Us!');
 });
 
-app.use(express.static(__dirname + '\\public')); 
+app.use(express.static(__dirname + '/public')); 
 //console.log(__dirname);
 
 app.listen(PORT, function (){
